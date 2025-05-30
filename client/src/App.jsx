22a08ace09898e60156889f3e9498d7a7f89486d
Alexander Shegstad <aShegstad@my365.Bellevue.edu>
2025-05-30 13:10:39 -0500

    ChakraProvider import and correct implmentation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
-import { Provider } from '@/components/ui/provider'
+//import { Provider } from '@/components/ui/provider'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <Provider>
-      <div as h1> App </div>
-    </Provider>
+      <div> App </div>
   )
 }
 
